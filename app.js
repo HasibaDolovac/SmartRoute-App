1ea2f2424c1ecec194ecbb8ef3d92b48136ae261
HasibaDolovac <dhasiba33@gmail.com>
2026-07-07 15:00:37 +0200

    kreiranje osnovnih migracija

--- app.js
+++ app.js
@@ -219,4 +219,4 @@ document.getElementById('searchInput').addEventListener('keydown', e => {
   if (e.key === 'Enter') searchLocation();
 });
 
- // konfiguracija razvojenog okruzenja i .env fajla 
+ // baza

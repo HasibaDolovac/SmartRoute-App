871b229dca4cb7dbb8d10a77aaa0d60fe5bd25d0
HasibaDolovac <dhasiba33@gmail.com>
2026-07-07 15:11:59 +0200

    resavanje problema

--- app.js
+++ app.js
@@ -219,4 +219,4 @@ document.getElementById('searchInput').addEventListener('keydown', e => {
   if (e.key === 'Enter') searchLocation();
 });
 
-//priprema za deployment .env podesavanja
+//resavanje problema 

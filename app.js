a58854067c3db138a9db283f04d00ab372eba910
HasibaDolovac <dhasiba33@gmail.com>
2026-07-07 15:05:07 +0200

    cuvanje rute u bazi podataka

--- app.js
+++ app.js
@@ -219,4 +219,4 @@ document.getElementById('searchInput').addEventListener('keydown', e => {
   if (e.key === 'Enter') searchLocation();
 });
 
- // iscrtavanje rute preko OpenRouteService API-ja
+//cuvanje rute u bazi podataka

a0d0951e2a597e34f5afac32f5344cb18664e0e2
HasibaDolovac <dhasiba33@gmail.com>
2026-07-07 15:06:15 +0200

    model i view za istoriju rute

--- app.js
+++ app.js
@@ -219,4 +219,4 @@ document.getElementById('searchInput').addEventListener('keydown', e => {
   if (e.key === 'Enter') searchLocation();
 });
 
-//cuvanje rute u bazi podataka
+//model i view za istoriju rute

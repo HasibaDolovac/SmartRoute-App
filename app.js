ea7131241b215058f677ba4c75abe7b356524047
HasibaDolovac <dhasiba33@gmail.com>
2026-07-07 14:53:09 +0200

    stilizovanje i responsive dizajn

--- app.js
+++ app.js
@@ -220,4 +220,4 @@ document.getElementById('searchInput').addEventListener('keydown', e => {
 });
 
 //registracija i prijava korisnika
-//admin panel pregled korisnika i ruta 
+// stilizovanje i responsive dizajn 
